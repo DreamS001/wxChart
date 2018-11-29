7b9ca702f2ef64f02ca23cdd5457a1c89fdac4e2
DreamS001 <1271486495@qq.com>
2018-11-29 11:56:46 +0800

    弹框组件

---
+++ components/Toastedit/toastedit.js
@@ -0,0 +1,46 @@
+// components/Toastedit/toastedit.js
+Component({
+  options: {
+    multipleSlots: true // 在组件定义时的选项中启用多slot支持
+  },
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    toastText:{
+      type:'string',
+      value:'内容'
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    isShow:false
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    showToast(text, time) {
+      this.setData({
+        toastShow: !this.data.toastShow,
+        toastText: text
+      })
+
+      var that = this
+
+      if (!time) {
+        time = 5000
+      }
+
+      setTimeout(function () {
+        that.setData({
+          toastShow: !that.data.toastShow
+        })
+      }, time)
+    }
+  }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,34 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+
+  /**
+ * 生命周期函数--监听页面初次渲染完成
+ */
+  onReady: function () {
+    //获得dialog组件
+    this.dialog = this.selectComponent("#dialog");
+    this.toastedit = this.selectComponent("#toastedit");
+  },
+
+  showDialog() {
+    this.dialog.showDialog();
+  },
+
+  //取消事件
+  _cancelEvent() {
+    console.log('你点击了取消');
+    this.dialog.hideDialog();
+  },
+  //确认事件
+  _confirmEvent() {
+    console.log('你点击了确定');
+    this.dialog.hideDialog();
+  },
+  showToast: function () {
+    this.toastedit.showToast('哇塞！成功了~', 2000)
+  }
+
+})
